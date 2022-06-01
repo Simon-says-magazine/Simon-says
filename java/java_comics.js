046958e1fe0f9b2520886ae30964afea8ca62b40
martasoricetti <92322269+martasoricetti@users.noreply.github.com>
2022-06-01 10:07:51 +0200

    Add files via upload

---
+++ java/java_comics.js
@@ -0,0 +1,65 @@
+/*metadata*/
+
+/* function to open and close footnotes*/
+function open_footnote_1() {
+    var node_1= document.getElementById("first_footnote");
+    if (node_1.style.display=="none") {
+        node_1.style.display="flex";
+    }
+    else
+    node_1.style.display= "none";
+}
+
+function open_footnote_2() {
+    var node_2= document.getElementById("second_footnote");
+    if (node_2.style.display=="none") {
+        node_2.style.display = "flex";
+    }
+    else
+    node_2.style.display= "none";
+}
+
+function open_footnote_3() {
+    var node_3= document.getElementById("third_footnote");
+    if (node_3.style.display=="none") {
+        node_3.style.display = "flex";
+    }
+    else
+    node_3.style.display= "none";
+}
+
+function open_footnote_4() {
+    var node_4= document.getElementById("fourth_footnote");
+    if (node_4.style.display=="none") {
+        node_4.style.display = "flex";
+    }
+    else
+    node_4.style.display= "none";
+}
+
+function open_footnote_5() {
+    var node_5= document.getElementById("fifth_footnote");
+    if (node_5.style.display=="none") {
+        node_5.style.display = "flex";
+    }
+    else
+    node_5.style.display= "none";
+}
+
+function open_footnote_6() {
+    var node_6= document.getElementById("sixth_footnote");
+    if (node_6.style.display=="none") {
+        node_6.style.display = "flex";
+    }
+    else
+    node_6.style.display= "none";
+}
+
+function open_footnote_7() {
+    var node_7= document.getElementById("seventh_footnote");
+    if (node_7.style.display=="none") {
+        node_7.style.display = "flex";
+    }
+    else
+    node_7.style.display= "none";
+}
